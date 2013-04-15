92ebad137c69082e9df5ec14b6661a6f1f19cd21
Armando <sor.arm@gmail.com>
2013-04-15 18:07:23 +0200

    Baseline1_15042013 Gruppo Giallo
    Baseline1_15042013 Gruppo Giallo

--- Rilasci/Baseline1_15042013/SQLScripts/2.SuperScript-cleanAll.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM editoria_gruppo_giallo.NOTIZIA;
-DELETE FROM editoria_gruppo_giallo.FUNZIONALITA;
-DELETE FROM editoria_gruppo_giallo.APPARTENENZA_GRUPPO;
-DELETE FROM editoria_gruppo_giallo.GRUPPO;
-DELETE FROM editoria_gruppo_giallo.ACCOUNT;
